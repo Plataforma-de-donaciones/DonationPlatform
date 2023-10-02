d86b839e1602601aca6b2de5d6de42dfd5abdb4e
FatimaCaballeroUY <fatima.caballero@estudiantes.utec.edu.uy>
2023-10-02 14:49:50 -0400

    Autenticacion con modelo de usuarios

--- donation_platform/users/models.py
+++ donation_platform/users/models.py
@@ -3,46 +3,64 @@ from donation_platform.models import Organization
 from django.utils import timezone
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+#class Users(models.Model):
+ #   user_name = models.CharField(max_length=50)
+  #  user_email = models.CharField(max_length=50, unique=True)
+   # user_password = models.TextField()
+    #organization = models.ForeignKey(Organization, models.DO_NOTHING, blank=True, null=True, related_name='users_organization')
+    #user_state = models.IntegerField()
+    #erased_at = models.DateTimeField(blank=True, null=True)
+    #erased_reason = models.TextField(blank=True, null=True)
+    #last_login = models.DateTimeField(default=timezone.now)
 
-class UsersManager(BaseUserManager):
-    def create_user(self, user_email, user_password, **extra_fields):
-        user = self.model(user_email=user_email, **extra_fields)
+    #class Meta:
+        #managed = False
+        #db_table = 'users'
+class CustomUserManager(BaseUserManager):
+    def create_user(self, user_name, user_email, user_password, **extra_fields):
+        if not user_email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(user_email)
+        user = self.model(user_name=user_name, user_email=email, **extra_fields)
         user.set_password(user_password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, user_email, user_password, **extra_fields):
+    def create_superuser(self, user_name, user_email, user_password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(user_name, user_email, user_password, **extra_fields)
 
-        return self.create_user(user_email, user_password, **extra_fields)
-
-class Users(AbstractBaseUser, PermissionsMixin):
-    user_name = models.CharField(max_length=50)
-    user_email = models.CharField(max_length=50, unique=True)
-    organization = models.ForeignKey(Organization, models.DO_NOTHING, blank=True, null=True, related_name='users_organization')
+class Users(AbstractBaseUser):
+    user_name = models.CharField(max_length=50, unique=True)
+    user_email = models.EmailField(unique=True)
+    user_password = models.TextField()
     user_state = models.IntegerField()
     erased_at = models.DateTimeField(blank=True, null=True)
     erased_reason = models.TextField(blank=True, null=True)
     last_login = models.DateTimeField(default=timezone.now)
 
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    objects = CustomUserManager()
 
-    objects = UsersManager()
+    USERNAME_FIELD = 'user_name'
+    EMAIL_FIELD = 'user_email'
+    REQUIRED_FIELDS = ['user_email', 'user_password']
 
-    USERNAME_FIELD = 'user_email'
-    REQUIRED_FIELDS = ['user_name']  # Agrega los campos requeridos adicionales aquí
+    def __str__(self):
+        return self.user_name
+
+    @property
+    def password(self):
+        return self.user_password
+
+    @password.setter
+    def password(self, value):
+        self.user_password = value
 
     class Meta:
-        managed = False
         db_table = 'users'
 
 class Administrator(models.Model):
